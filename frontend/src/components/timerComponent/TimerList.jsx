a6ef48ef2502f01362e1a0dba8d2b886a864ff54
ajay kumar <ajaykumar1071999@gmail.com>
2024-04-18 13:14:30 +0530

    Ajay initial commit

--- frontend/src/components/timerComponent/TimerList.jsx
+++ frontend/src/components/timerComponent/TimerList.jsx
@@ -1,22 +1,24 @@
-import React, {useState} from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { timerSelector, removeTimer, togglePause } from '../../redux/reducers/timerSlice'
-import { setHelper } from '../../redux/reducers/apiHelperSlice'
-import Timer from './Timer'
-import axios from 'axios'
-import "./timerList.scss"
-import Loader from '../../utils/Loader'
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  timerSelector,
+  removeTimer,
+  togglePause,
+} from "../../redux/reducers/timerSlice";
+import { setHelper } from "../../redux/reducers/apiHelperSlice";
+import Timer from "./Timer";
+import axios from "axios";
+import "./timerList.scss";
+import Loader from "../../utils/Loader";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const TimerList = () => {
-
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-
-  const state = useSelector(timerSelector)
-  const dispatch = useDispatch()
+  const state = useSelector(timerSelector);
+  const dispatch = useDispatch();
 
   const formatTime = (timeLeft) => {
     const minutes = Math.floor(timeLeft / 60);
@@ -24,90 +26,87 @@ const TimerList = () => {
     return `${minutes} minutes ${seconds} seconds`;
   };
 
-  const randomNumber = parseInt(Math.random() * 10000000)
+  const randomNumber = parseInt(Math.random() * 10000000);
 
   const removeTimerHandler = async (id, timeLeft) => {
-      
-
-      try {
-
-        const data = { timerId: id, activeDuration:formatTime(timeLeft) , isDeleted: true, deleteDate: true }
-        setLoading(true)
-        await axios.put(`${BASE_URL}/timer`, data)
-        dispatch(removeTimer({id: id}))
-        dispatch(setHelper(id+randomNumber))
+    try {
+      const data = {
+        timerId: id,
+        activeDuration: formatTime(timeLeft),
+        isDeleted: true,
+        deleteDate: true,
+      };
+      setLoading(true);
+      await axios.put(`${BASE_URL}/timer`, data);
+      dispatch(removeTimer({ id: id }));
+      dispatch(setHelper(id + randomNumber));
+      setLoading(false);
+    } catch (error) {
+      if (error.response) {
+        setError(error.response.data.message);
         setLoading(false);
-        
-      } catch (error) {
-        if (error.response) {
-
-          setError(error.response.data.message);
-          setLoading(false);
-  
-        } else if (error.request) {
-  
-          setError(error.request);
-          setLoading(false);
-  
-        } else {
-    
-          setError(error.message);
-          setLoading(false);
-        }
-      }
-  
-      
-  }
-
-  const pauseTimerHandler = async (id, timeLeft) => { 
-      try {
-
-        const data = { timerId: id, activeDuration: formatTime(timeLeft), timeLeft: timeLeft }
-        setLoading(true)
-        await axios.put(`${BASE_URL}/timer`, data)
-        dispatch(togglePause({id: id}))
-        dispatch(setHelper(id+randomNumber))
+      } else if (error.request) {
+        setError(error.request);
         setLoading(false);
-        
-      } catch (error) {
-        if (error.response) {
-
-          setError(error.response.data.message);
-          setLoading(false);
-  
-        } else if (error.request) {
-  
-          setError(error.request);
-          setLoading(false);
-  
-        } else {
-    
-          setError(error.message);
-          setLoading(false);
-        }
+      } else {
+        setError(error.message);
+        setLoading(false);
       }
-      
-  }
+    }
+  };
+
+  const pauseTimerHandler = async (id, timeLeft) => {
+    try {
+      const data = {
+        timerId: id,
+        activeDuration: formatTime(timeLeft),
+        timeLeft: timeLeft,
+      };
+      setLoading(true);
+      await axios.put(`${BASE_URL}/timer`, data);
+      dispatch(togglePause({ id: id }));
+      dispatch(setHelper(id + randomNumber));
+      setLoading(false);
+    } catch (error) {
+      if (error.response) {
+        setError(error.response.data.message);
+        setLoading(false);
+      } else if (error.request) {
+        setError(error.request);
+        setLoading(false);
+      } else {
+        setError(error.message);
+        setLoading(false);
+      }
+    }
+  };
 
   if (error) {
     return <div>{error}</div>;
   }
 
   return (
-    
-    <div className='currentTimerList'>
-        <h3>Current Timer List</h3>
-        <hr/>
-        {loading? <Loader/> : state && state.map(item=> <Timer key={item.id} 
-                                          id={item.id}
-                                          name={item.timerName} 
-                                          startTime={item.totalDuration} 
-                                          isPause={item.isPause}
-                                          removeTimerHandler={removeTimerHandler}
-                                          pauseTimer={pauseTimerHandler}/>)}
+    <div className="currentTimerList">
+      <h3>Current Timer List</h3>
+      <hr />
+      {loading ? (
+        <Loader />
+      ) : (
+        state &&
+        state.map((item) => (
+          <Timer
+            key={item.id}
+            id={item.id}
+            name={item.timerName}
+            startTime={item.totalDuration}
+            isPause={item.isPause}
+            removeTimerHandler={removeTimerHandler}
+            pauseTimer={pauseTimerHandler}
+          />
+        ))
+      )}
     </div>
+  );
+};
 
-  )
-}
-
-export default TimerList
+export default TimerList;
